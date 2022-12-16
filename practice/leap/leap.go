1f35c736dd88db1bb8b0fcb38e1d2dbaab9b17aa
XinceChan <96606103+XinceChan@users.noreply.github.com>
2022-12-16 21:08:49 +0800

    finish leap

---
+++ practice/leap/leap.go
@@ -0,0 +1,11 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package leap should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package leap
+
+// IsLeapYear should have a comment documenting it.
+func IsLeapYear(i int) bool {
+	return i%4 == 0 && i%100 != 0 || i%400 == 0
+}
